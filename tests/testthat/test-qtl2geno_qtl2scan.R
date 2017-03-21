ce5b1562eabf366846c56d79bf01314ea79a4bc7
Karl Broman <kbroman@gmail.com>
2017-03-21 15:40:23 -0500

    Add tests

---
+++ tests/testthat/test-qtl2geno_qtl2scan.R
@@ -0,0 +1,99 @@
+context("qtl2geno/qtl2scan")
+
+test_that("feather_genoprob works with qtl2geno/qtl2scan functions", {
+
+    library(qtl2geno)
+    iron <- read_cross2(system.file("extdata", "iron.zip", package="qtl2geno"))
+    iron <- iron[,c(18,19,"X")]
+
+    map <- insert_pseudomarkers(iron$gmap, step=1)
+    probs <- calc_genoprob(iron, map, error_prob=0.002)
+
+    dir <- tempdir()
+    fprobs <- feather_genoprob(probs, "iron_probs", dir)
+
+    # get the same thing when subsetting
+    expect_equal(fprobs[["18"]], probs[["18"]])
+    expect_equal(fprobs[[1]], probs[["18"]])
+    expect_equal(fprobs[["X"]], probs[["X"]])
+    expect_equal(fprobs[[3]], probs[["X"]])
+
+    # calc_kinship
+    expect_equal(calc_kinship(probs), calc_kinship(fprobs))
+    expect_equal(calc_kinship(probs, "loco"), calc_kinship(fprobs, "loco"))
+    expect_equal(calc_kinship(probs, "chr"), calc_kinship(fprobs, "chr"))
+
+    # genoprob_to_alleleprob
+    expect_equal(genoprob_to_alleleprob(probs), genoprob_to_alleleprob(fprobs))
+
+    # probs_to_grid
+    grid <- calc_grid(iron$gmap, step=1)
+    expect_equal(probs_to_grid(probs, grid), probs_to_grid(fprobs, grid))
+
+    # scan1
+    Xcovar <- get_x_covar(iron)
+    sex <- Xcovar[,"sex",drop=FALSE]
+    library(qtl2scan)
+    expect_equal(scan1(probs, iron$pheno, Xcovar=Xcovar),
+                 scan1(fprobs, iron$pheno, Xcovar=Xcovar))
+    expect_equal(scan1(probs, iron$pheno, Xcovar=Xcovar, addcovar=sex),
+                 scan1(fprobs, iron$pheno, Xcovar=Xcovar, addcovar=sex))
+    expect_equal(scan1(probs, iron$pheno, Xcovar=Xcovar, addcovar=sex, intcovar=sex),
+                 scan1(fprobs, iron$pheno, Xcovar=Xcovar, addcovar=sex, intcovar=sex))
+
+    # scan1 with kinship
+    k <- calc_kinship(probs)
+    fk <- calc_kinship(fprobs)
+    expect_equal(scan1(probs, iron$pheno, k, Xcovar=Xcovar),
+                 scan1(fprobs, iron$pheno, fk, Xcovar=Xcovar))
+    expect_equal(scan1(probs, iron$pheno, k, Xcovar=Xcovar, addcovar=sex),
+                 scan1(fprobs, iron$pheno, fk, Xcovar=Xcovar, addcovar=sex))
+    expect_equal(scan1(probs, iron$pheno, k, Xcovar=Xcovar, addcovar=sex, intcovar=sex),
+                 scan1(fprobs, iron$pheno, fk, Xcovar=Xcovar, addcovar=sex, intcovar=sex))
+
+    # scan1 with "loco" kinship
+    k <- calc_kinship(probs, "loco")
+    fk <- calc_kinship(fprobs, "loco")
+    expect_equal(scan1(probs, iron$pheno, k, Xcovar=Xcovar),
+                 scan1(fprobs, iron$pheno, fk, Xcovar=Xcovar))
+    expect_equal(scan1(probs, iron$pheno, k, Xcovar=Xcovar, addcovar=sex),
+                 scan1(fprobs, iron$pheno, fk, Xcovar=Xcovar, addcovar=sex))
+    expect_equal(scan1(probs, iron$pheno, k, Xcovar=Xcovar, addcovar=sex, intcovar=sex),
+                 scan1(fprobs, iron$pheno, fk, Xcovar=Xcovar, addcovar=sex, intcovar=sex))
+
+
+    # scan1coef
+    phe <- iron$pheno[,1,drop=FALSE]
+    expect_equal(scan1coef(subset(probs, chr="18"), phe, addcovar=sex),
+                 scan1coef(subset(fprobs, chr="18"), phe, addcovar=sex))
+    expect_equal(scan1coef(subset(probs, chr="X"), phe, addcovar=Xcovar),
+                 scan1coef(subset(fprobs, chr="X"), phe, addcovar=Xcovar))
+
+    # scan1coef with kinship
+    phe <- iron$pheno[,1,drop=FALSE]
+    expect_equal(scan1coef(subset(probs, chr="18"), phe, k[["18"]], addcovar=sex),
+                 scan1coef(subset(fprobs, chr="18"), phe, fk[["18"]], addcovar=sex))
+    expect_equal(scan1coef(subset(probs, chr="X"), phe, k[["X"]], addcovar=Xcovar),
+                 scan1coef(subset(fprobs, chr="X"), phe, fk[["X"]], addcovar=Xcovar))
+
+    # scan1blup
+    phe <- iron$pheno[,1,drop=FALSE]
+    expect_equal(scan1blup(subset(probs, chr="18"), phe, addcovar=sex),
+                 scan1blup(subset(fprobs, chr="18"), phe, addcovar=sex))
+    expect_equal(scan1blup(subset(probs, chr="X"), phe, addcovar=Xcovar),
+                 scan1blup(subset(fprobs, chr="X"), phe, addcovar=Xcovar))
+
+    # scan1blup with kinship
+    phe <- iron$pheno[,1,drop=FALSE]
+    expect_equal(scan1blup(subset(probs, chr="18"), phe, k[["18"]], addcovar=sex),
+                 scan1blup(subset(fprobs, chr="18"), phe, fk[["18"]], addcovar=sex))
+    expect_equal(scan1blup(subset(probs, chr="X"), phe, k[["X"]], addcovar=Xcovar),
+                 scan1blup(subset(fprobs, chr="X"), phe, fk[["X"]], addcovar=Xcovar))
+
+
+
+    # clean up
+    lf <- list.files(dir, pattern=".feather")
+    unlink(file.path(dir, lf))
+
+})
